fb53348566c358a3b24920ae026cf32411204ee4
Joaquin Zandomeni <49771018+Zandomeni@users.noreply.github.com>
2023-05-30 14:14:16 -0300

    Update required-FK.sql

--- scrpits/required-FK.sql
+++ scrpits/required-FK.sql
@@ -9,3 +9,7 @@ FOREIGN KEY (FKEscu) REFERENCES Escuela(NroEscu);
 ALTER TABLE Expediente
 ADD CONSTRAINT Cargo_Expe /* Cargo tiene una FK en Expediente */
 FOREIGN KEY (FKCargo) REFERENCES Cargo(NroCargo);
+
+ALTER TABLE Expediente
+ADD CONSTRAINT Suple_Expe /* Docente tiene una segunda FK en Expediente */
+FOREIGN KEY (FKSuplente) REFERENCES Docente(DNI);
